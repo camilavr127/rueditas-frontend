3c8b886471bfc42f803b1eccebee90b80b6d1149
camilavr127 <dhayanavr@gmail.com>
2024-09-24 18:40:55 -0500

    Commit inicial

---
+++ src/main/java/com/t1/rueda_front/RuedaFrontApplication.java
@@ -0,0 +1,13 @@
+package com.t1.rueda_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuedaFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuedaFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t1/rueda_front/RuedaFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t1.rueda_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuedaFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
